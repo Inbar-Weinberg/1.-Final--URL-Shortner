6590121072bfdb2fba00eb794135808e1becd99c
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-03-07 00:26:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const cors = require("cors");
+const fsPromises = require("fs/promises");
+
+const express = require("express");
+const app = express();
+const api = require("./api");
+let dataBase;
+
+app.set("view engine", "ejs");
+app.use(cors());
+app.use("/public", express.static(`./public`));
+//-- use after Listen.start
+app.use("/", (req, res, next) => {
+  req.dataBase = require(`${process.cwd()}/classes/dataBaseController`).dataBase;
+  next();
+});
+
+app.get("/", (req, res) => {
+  const allUrls = req.dataBase.getAllElements();
+  res.render("index", {
+    allUrls,
+    status: req.query.status,
+    url: req.query.message,
+  });
+});
+
+app.use("/api", api);
+
+module.exports = app;
